ac9d6d2c63f6586579ec118f95b7821f790f2ea2
Scott Knight <sknight@Scotts-MacBook-Stauffer.local>
2013-07-02 11:09:31 -0700

    href fix on hero

--- drupal/sites/all/themes/pivot/templates/views-view-unformatted--homepage-slider--block.tpl.php
+++ drupal/sites/all/themes/pivot/templates/views-view-unformatted--homepage-slider--block.tpl.php
@@ -5,8 +5,10 @@
         <?php foreach ($view->result as $id => $tab): ?>
           <?php $node = node_load($tab->nid); ?>
           <?php $tab = $node->title; ?>
-           <li class="item <?php print $node->title; ?>">
-              <?php $title = str_replace(' ', '_', strtolower($node->title)); ?>
+          <?php $title = str_replace(' ', '-', strtolower($tab)); ?>
+          <?php $title = preg_replace("/[^a-zA-Z0-9\s]/", "", $title); ?>
+           <li class="item <?php print $title; ?>">
+              
               <a href='#<?php print $title; ?>'><?php print $tab; ?></a>
             </li>
         <?php endforeach; ?>
@@ -14,10 +16,18 @@
       <div class="active-selector"></div>
     </nav>
     <div class="swiper-wrapper slide-wrapper">
-      <?php foreach ($rows as $id => $row): ?>
-        <div class="<?php print $classes_array[$id]; ?>">
-          <?php print $row; ?>
-        </div>
+      <?php foreach ($rows as $id => $row):
+      $nid = $view->result[$id]->nid;
+      $node_tab = node_load($nid);
+      $title_tab = $node_tab->title;
+      $title_tab = str_replace(' ', '-', strtolower($title_tab));
+      $title_tab = preg_replace("/[^a-zA-Z0-9\s]/", "", $title_tab);
+      ?>
+        <article id="<?php print $title_tab; ?>" class="slide" data-token="<?php print $title_tab; ?>">
+          <div class="<?php print $classes_array[$id]; ?>">
+            <?php print $row; ?>
+          </div>
+        </article>
       <?php endforeach; ?>
     </div>
   </span>
