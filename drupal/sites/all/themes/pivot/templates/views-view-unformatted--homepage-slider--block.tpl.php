3447f4d62ad3e864c8ea065437c737294af06bc9
Scott Knight <sknight@Scotts-MacBook-Stauffer.local>
2013-07-02 11:24:58 -0700

    images for the hero menu

--- drupal/sites/all/themes/pivot/templates/views-view-unformatted--homepage-slider--block.tpl.php
+++ drupal/sites/all/themes/pivot/templates/views-view-unformatted--homepage-slider--block.tpl.php
@@ -5,10 +5,8 @@
         <?php foreach ($view->result as $id => $tab): ?>
           <?php $node = node_load($tab->nid); ?>
           <?php $tab = $node->title; ?>
-          <?php $title = str_replace(' ', '-', strtolower($tab)); ?>
-          <?php $title = preg_replace("/[^a-zA-Z0-9\s]/", "", $title); ?>
+          <?php $title = str_replace(' ', '_', strtolower($tab)); ?>
            <li class="item <?php print $title; ?>">
-              
               <a href='#<?php print $title; ?>'><?php print $tab; ?></a>
             </li>
         <?php endforeach; ?>
@@ -20,8 +18,7 @@
       $nid = $view->result[$id]->nid;
       $node_tab = node_load($nid);
       $title_tab = $node_tab->title;
-      $title_tab = str_replace(' ', '-', strtolower($title_tab));
-      $title_tab = preg_replace("/[^a-zA-Z0-9\s]/", "", $title_tab);
+      $title_tab = str_replace(' ', '_', strtolower($title_tab));
       ?>
         <article id="<?php print $title_tab; ?>" class="slide" data-token="<?php print $title_tab; ?>">
           <div class="<?php print $classes_array[$id]; ?>">
