7bdb0666ab5d11b9fa0cf7e7ee6a40d29cc0a698
Scott Knight <sknight@Scotts-MacBook-Stauffer.local>
2013-07-10 16:06:43 -0700

    added the important class to the more link

--- drupal/sites/all/themes/pivot/templates/node--hero.tpl.php
+++ drupal/sites/all/themes/pivot/templates/node--hero.tpl.php
@@ -5,7 +5,7 @@
   $image_path = file_create_url($variables['field_hs_slider_image'][0]['uri']);
   $more_link = '&nbsp;';
   if (isset($variables['field_hs_link'][0]['title']) && isset($variables['field_hs_link'][0]['url'])) {
-    $more_link = l($variables['field_hs_link'][0]['title'], $variables['field_hs_link'][0]['url']);
+    $more_link = l($variables['field_hs_link'][0]['title'], $variables['field_hs_link'][0]['url'], array('attributes' => array('class' => 'important')));
   }
   $output .= '<div class="video-content">
           <div class="video-content-inner">
@@ -21,9 +21,4 @@
         </div>';
 
   print $output;
-    // drupal_add_js(drupal_get_path('module', 'pivot_hero'). '/js/jquery.scrollTo.js');
-    // drupal_add_js(drupal_get_path('module', 'pivot_hero'). '/js/tpslide.jquery.js');
-    // drupal_add_js(drupal_get_path('module', 'pivot_hero'). '/js/tpmodal.jquery.js');
-    // drupal_add_js(drupal_get_path('module', 'pivot_hero'). '/js/tpselect.jquery.js');
-    // drupal_add_js(drupal_get_path('module', 'pivot_hero'). '/js/pivot-hero-script.js');
 ?>
