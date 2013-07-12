d7be6b57f907b0d47220504eaf8b009919e44c52
Charles Toepfer <charles.toepfer@gmail.com>
2013-07-11 18:04:36 -0700

    added getValOnce and getQueryParam to sitecatalyst JS

--- drupal/sites/all/libraries/sitecatalyst/s_code_h25_core.js
+++ drupal/sites/all/libraries/sitecatalyst/s_code_h25_core.js
@@ -30,6 +30,33 @@ if (!s.campaign) {
     s.campaign = s.campaign.toLowerCase();
 }
 
+/************************** PLUGINS SECTION *************************/
+
+/*
+ * Plugin: getQueryParam 2.3
+ */
+s.getQueryParam=new Function("p","d","u",""
++"var s=this,v='',i,t;d=d?d:'';u=u?u:(s.pageURL?s.pageURL:s.wd.locati"
++"on);if(u=='f')u=s.gtfs().location;while(p){i=p.indexOf(',');i=i<0?p"
++".length:i;t=s.p_gpv(p.substring(0,i),u+'');if(t){t=t.indexOf('#')>-"
++"1?t.substring(0,t.indexOf('#')):t;}if(t)v+=v?d+t:t;p=p.substring(i="
++"=p.length?i:i+1)}return v");
+s.p_gpv=new Function("k","u",""
++"var s=this,v='',i=u.indexOf('?'),q;if(k&&i>-1){q=u.substring(i+1);v"
++"=s.pt(q,'&','p_gvf',k)}return v");
+s.p_gvf=new Function("t","k",""
++"if(t){var s=this,i=t.indexOf('='),p=i<0?t:t.substring(0,i),v=i<0?'T"
++"rue':t.substring(i+1);if(p.toLowerCase()==k.toLowerCase())return s."
++"epa(v)}return ''");
+
+/*
+ * Plugin: getValOnce 0.2
+ */
+s.getValOnce=new Function("v","c","e",""
++"var s=this,k=s.c_r(c),a=new Date;e=e?e:0;if(v){a.setTime(a.getTime("
++")+e*86400000);s.c_w(c,v,e?a:0);}return v==k?'':v");
+
+
 /* WARNING: Changing any of the below variables will cause drastic
 changes to how your visitor data is collected.  Changes should only be
 made when instructed to do so by your account manager.*/
