d9d7f67b670937d0dda3ff20b25429dbd67b663e
Matthew Wrather <matt@wrathercreative.com>
2013-07-11 17:06:28 -0700

    added paid-account webfont snippet

--- drupal/sites/all/themes/pivot/template.php
+++ drupal/sites/all/themes/pivot/template.php
@@ -130,7 +130,8 @@ function STARTERKIT_preprocess_maintenance_page(&$variables, $hook) {
  */
 function pivot_preprocess_html(&$variables, $hook) {
 
-  drupal_add_css('//f.fontdeck.com/s/css/ANxYhj9dllkYxm1NMwN3LihqgYU/' . $_SERVER['SERVER_NAME'] . '/34611.css', array('type' => 'external', 'group' => CSS_THEME, 'every_page' => TRUE));
+  drupal_add_js("WebFontConfig = { fontdeck: { id: '35228' } };", array('type' => 'inline', 'scope'=> 'footer', 'weight' => 10));
+  drupal_add_js('//ajax.googleapis.com/ajax/libs/webfont/1/webfont.js', array('type' => 'external', 'scope' => 'footer', 'weight' => 11));
 
   // The body tag's classes are controlled by the $classes_array variable. To
   // remove a class from $classes_array, use array_diff().
