6454514177135a9729d9cc24a65c3b685a0b993f
Scott Knight <sknight@Scotts-MacBook-Stauffer.local>
2013-07-18 13:47:36 -0700

    campaign hero block cleanup

---
+++ drupal/sites/all/modules/custom/pivot_campaign/pivot-campaign-hero.tpl.php
@@ -0,0 +1,17 @@
+<div class="site-wrapper">
+	<div class="show-hero">
+		<h1 class="headline"><?php print $headline_link; ?></h1>
+		<div class="image">
+			<?php print $hero_image; ?>
+      <a class='hero-ad' href="<?php print $ad_link; ?>">
+        <?php print $ad_image; ?>
+      </a>
+		</div>
+	</div>
+	<nav class="nav">
+		<h2 class="headline">Show Navigation</h2>
+		<ul>
+      <?php print $hero_menu; ?>
+		</ul>
+	</nav>
+</div>
