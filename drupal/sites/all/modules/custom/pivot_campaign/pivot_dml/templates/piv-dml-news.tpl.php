579b8abfdce560207ac8ffbe7331bd3d29930365
Scott Knight <sknight17@gmail.com>
2013-07-23 12:27:18 -0700

    updated dml news page

--- drupal/sites/all/modules/custom/pivot_campaign/pivot_dml/templates/piv-dml-news.tpl.php
+++ drupal/sites/all/modules/custom/pivot_campaign/pivot_dml/templates/piv-dml-news.tpl.php
@@ -1,25 +1,2 @@
-<div id="page">
-  <?php
-    $header = render($page['header']);
-    if ($header):
-  ?>
-  <header id="header" role="banner">
-    <?php print $header; ?>
-  </header>
-  <?php endif; ?>
-  <main id="main">
-    <div id="content" class="site-wrapper">
-
-
-
     <!-- CONTENT GOES HERE -->
     <h1>News Page</h1>
-
-
-
-
-    </div>
-  </main><!-- /#main -->
-  <?php // footer will always render ?>
-  <?php print render($page['footer']); ?>
-</div><!-- /#page -->
