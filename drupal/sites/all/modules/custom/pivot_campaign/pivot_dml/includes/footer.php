f04a0079efeef3c70e4f8c3a859f8b5c791b923d
Scott Knight <sknight17@gmail.com>
2013-07-25 12:53:25 -0700

    added facebook sdk to libraries and added dml sdk to pivot_dml

--- drupal/sites/all/modules/custom/pivot_campaign/pivot_dml/includes/footer.php
+++ drupal/sites/all/modules/custom/pivot_campaign/pivot_dml/includes/footer.php
@@ -1,35 +1,20 @@
-  		
+<script type="text/javascript" src="//platform.twitter.com/widgets.js"></script>
 
-		</div><!-- /.page-wrap -->
-		
-		
-		<script src="/<?php echo pivot_dml_get_path('templates'); ?>/public/js/vendor/modernizr-2.6.2.min.js"></script>
-		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
-        <script>window.jQuery || document.write('<script src="/<?php echo pivot_dml_get_path('templates'); ?>/public/js/vendor/jquery-1.9.1.min.js"><\/script>')</script>
-		<!--[if lt IE 9]>
-		<script src="/<?php echo pivot_dml_get_path('templates'); ?>/public/js/vendor/html5shiv.js"></script>
-		<![endif]-->
+<script type="text/javascript">
+	window.fbAsyncInit = function() {
+		FB.init({
+	 		appId: 545435132181638,
+	 		status: true,
+	 		cookie: true,
+	 		xfbml: true
+	  	});
+	};
+	//Load the SDK asynchronously
+	(function() {
+	  var e = document.createElement('script');
+	  e.async = true;
+	  e.src = document.location.protocol + '//connect.facebook.net/en_US/all.js';
+	  document.getElementById('fb-root').appendChild(e);
+	}());
+</script>
 
-		<script type="text/javascript" src="//platform.twitter.com/widgets.js"></script>
-		
-		<script type="text/javascript">
-			window.fbAsyncInit = function() {
-				FB.init({
-			 		appId: 545435132181638,
-			 		status: true,
-			 		cookie: true,
-			 		xfbml: true
-			  	});
-			};
-			//Load the SDK asynchronously
-			(function() {
-			  var e = document.createElement('script');
-			  e.async = true;
-			  e.src = document.location.protocol + '//connect.facebook.net/en_US/all.js';
-			  document.getElementById('fb-root').appendChild(e);
-			}());
-		</script>
-
-        <script src="/<?php echo pivot_dml_get_path('templates'); ?>/public/js/plugins.js"></script>
-        <script src="/<?php echo pivot_dml_get_path('templates'); ?>/public/js/main.js"></script>
-		
