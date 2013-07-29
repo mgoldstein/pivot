a07c147b5bae3ba4b54013b711a38e0dd712d3c9
Scott Knight <sknight17@gmail.com>
2013-07-29 09:48:38 -0700

    themed the shows header

--- drupal/sites/all/modules/custom/pivot_shows/pivot-shows-hero.tpl.php
+++ drupal/sites/all/modules/custom/pivot_shows/pivot-shows-hero.tpl.php
@@ -1,14 +1,14 @@
-<div class="site-wrapper">
-	<div class="show-hero">
-		<h1 class="headline"><?php print $headline_link; ?></h1>
-		<div class="image">
-			<?php print $hero_image; ?>
+<header id="show-header" class="hero-header">
+	<div class="site-wrapper">
+		<div class="show-hero">
+			<h1 class="headline"><?php print $headline_link; ?></h1>
+			<div class="image">
+				<?php print $hero_image; ?>
+			</div>
 		</div>
+		<nav class="nav">
+			<h2 class="headline">Show Navigation</h2>
+	    <?php print $hero_menu; ?>
+		</nav>
 	</div>
-	<nav class="nav">
-		<h2 class="headline">Show Navigation</h2>
-		<ul>
-      <?php print $hero_menu; ?>
-		</ul>
-	</nav>
-</div>
+</header>
