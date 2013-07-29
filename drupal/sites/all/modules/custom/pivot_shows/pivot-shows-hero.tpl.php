b14416039a6803cdeb6571984572669132f7adbe
Scott Knight <sknight17@gmail.com>
2013-07-29 09:09:45 -0700

    added show module

---
+++ drupal/sites/all/modules/custom/pivot_shows/pivot-shows-hero.tpl.php
@@ -0,0 +1,14 @@
+<div class="site-wrapper">
+	<div class="show-hero">
+		<h1 class="headline"><?php print $headline_link; ?></h1>
+		<div class="image">
+			<?php print $hero_image; ?>
+		</div>
+	</div>
+	<nav class="nav">
+		<h2 class="headline">Show Navigation</h2>
+		<ul>
+      <?php print $hero_menu; ?>
+		</ul>
+	</nav>
+</div>
