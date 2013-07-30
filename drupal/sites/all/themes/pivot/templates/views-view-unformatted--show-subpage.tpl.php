e5cb09ab07b2fc9c1dbde79e00c40bc5361b4570
Matthew Wrather <matt@wrathercreative.com>
2013-07-29 22:33:14 -0700

    Update Cast View and Template

--- drupal/sites/all/themes/pivot/templates/views-view-unformatted--show-subpage.tpl.php
+++ drupal/sites/all/themes/pivot/templates/views-view-unformatted--show-subpage.tpl.php
@@ -6,24 +6,27 @@
  *
  * @ingroup views_templates
  */
+
 ?>
 <?php if (!empty($title)): ?>
   <h3><?php print $title; ?></h3>
 <?php endif; ?>
-  <div class="article-sidebar">
+  <div class="show-sidebar">
     <aside class="social tpsticky_main above"><div class="inner">
       <div class="tp-social"></div>
       <div id="article-social-more" class="article-social-more">
-  <h4 class="trigger"><a href="#article-more-shares">More</a></h4>
-  <section id="article-more-shares" class="article-more-shares">
-    <h5 class="headline">Share with your friends</h5>
-    <p></p>
-  </section>
+        <h4 class="trigger"><a href="#article-more-shares">More</a></h4>
+        <section id="article-more-shares" class="article-more-shares">
+          <h5 class="headline">Share with your friends</h5>
+          <p></p>
+        </section>
       </div>
-    </aside>
+    </div></aside>
   </div>
-<?php foreach ($rows as $id => $row): ?>
-  <div<?php if ($classes_array[$id]) { print ' class="' . $classes_array[$id] .'"';  } ?>>
-    <?php print $row; ?>
+  <div class="view-content-content">
+  <?php foreach ($rows as $id => $row): ?>
+    <div<?php if ($classes_array[$id]) { print ' class="' . $classes_array[$id] .'"';  } ?>>
+      <?php print $row; ?>
+    </div>
+  <?php endforeach; ?>
   </div>
-<?php endforeach; ?>
