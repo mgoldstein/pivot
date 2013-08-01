8624263f3e5ae3e2e8daba04a30b1483511a8b7e
JP McDowell <jp@takepart.com>
2013-08-01 13:42:06 -0700

    PIV-38: Fixed newsletter analytics to use the existing AJAX tigger in the signup reponse

--- drupal/sites/all/modules/custom/pivot_custom_analytics/js/newsletter_campaign.js
+++ drupal/sites/all/modules/custom/pivot_custom_analytics/js/newsletter_campaign.js
@@ -1,12 +1,16 @@
-(function($) {
-  $.fn.analytics_ncs = function(errors) {
-    if(errors == null) {
-	s.linkTrackVars = "eVar22,eVar23,eVar30,events";
+(function ($) {
+  $(document).ready(function() {
+    $("body").bind(
+      'newsletter_social_signup', function(e, title) {
+        s.linkTrackVars = "eVar22,eVar23,eVar30,events";
         s.linkTrackEvents = "event39";
-        s.events = "event39";
-        s.eVar22 = "Right Rail Signup";
-	s.tl(this, "o", s.eVar22);
-	_gaq.push(['_trackEvent', 'Social Shares', 'Click', s.eVar22]);
-    }
-  };
+        s.eVar22 = 'Right Rail Signup';
+        s.eVar23 = title;
+        s.eVar30 = s.pageName;
+        s.events = 'event39';
+        s.tl(true, 'o', 'Newsletter Signup');
+        _gaq.push(['_trackEvent', 'Social Shares', 'Click', s.eVar22]);
+      }
+    );
+  });
 })(jQuery);
