d9a663d032f104af55a6069175c6b172aeb8a18f
Matthew Wrather <matt@wrathercreative.com>
2013-07-31 17:38:17 -0700

    Add pivot site blog header functionality

--- drupal/sites/all/modules/custom/pivot_campaign/pivot-campaign-hero.tpl.php
+++ drupal/sites/all/modules/custom/pivot_campaign/pivot-campaign-hero.tpl.php
@@ -1,15 +1,12 @@
-<div class="site-wrapper">
-	<div class="show-hero">
+ <div class="site-wrapper">
+	<div class="campaign-hero">
 		<h1 class="headline"><?php print $headline_link; ?></h1>
 		<div class="image">
 			<?php print $hero_image; ?>
-      <a class='hero-ad' href="<?php print $ad_link; ?>">
-        <?php print $ad_image; ?>
-      </a>
 		</div>
 	</div>
 	<nav class="nav">
-		<h2 class="headline">Show Navigation</h2>
+		<h2 class="headline">Campaign Navigation</h2>
 		<ul>
       <?php print $hero_menu; ?>
 		</ul>
---
+++ drupal/sites/all/modules/custom/pivot_campaign/pivot-site-blog-hero.tpl.php
@@ -0,0 +1,5 @@
+<div class="pivot-site-blog-hero">
+	<div class="image">
+		<?php print $hero_image; ?>
+	</div>
+</div>
