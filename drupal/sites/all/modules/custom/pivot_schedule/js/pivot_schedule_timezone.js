ef337003ba1e934561adf4edaad985b0f6eb7108
Scott Knight <sknight17@gmail.com>
2013-08-26 09:28:23 -0700

    initial development for the schedule page

---
+++ drupal/sites/all/modules/custom/pivot_schedule/js/pivot_schedule_timezone.js
@@ -0,0 +1,17 @@
+/**
+ * @file
+ * Determine and set user's timezone on page load.
+ */
+jQuery(document).ready(function () {
+
+  // Determine timezone from browser client using jsTimezoneDetect library.
+  var tz = jstz.determine();
+  // Post timezone to callback url via ajax.
+  jQuery.ajax({
+    type: 'POST',
+    url: '/pivot-ajax/timezone',
+    dataType: 'json',
+    data: 'timezone=' + tz.name()
+  });
+
+});
