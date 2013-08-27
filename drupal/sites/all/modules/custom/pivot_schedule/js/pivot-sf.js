20d13d431edbe1f292d803913dc4f4920216f28d
Scott Knight <sknight17@gmail.com>
2013-08-27 14:08:23 -0700

    themed date filter

---
+++ drupal/sites/all/modules/custom/pivot_schedule/js/pivot-sf.js
@@ -0,0 +1,16 @@
+/**
+ * @file
+ * Determine and set user's timezone on page load.
+ */
+jQuery(document).ready(function () {
+
+    jQuery('ul.sfw-menu').superfish({
+      delay:       500,                            // one second delay on mouseout
+      animation:   {opacity:'show',height:'show'},  // fade-in and slide-down animation
+      speed:       'slow',                          // faster animation speed
+      autoArrows:  false,                            // disable generation of arrow mark-up
+      depth:        '-1',
+      respect_expanded:  false
+    });
+
+});
