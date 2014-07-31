6f9cd29a423587bb684b2177e0039c6192b62c11
Scott Knight <sknight17@gmail.com>
2014-07-31 10:57:10 -0700

    removed bottom link

--- drupal/sites/all/modules/custom/pivot_interstitial/pivot-interstitial-channel-finder.tpl.php
+++ drupal/sites/all/modules/custom/pivot_interstitial/pivot-interstitial-channel-finder.tpl.php
@@ -1,15 +1,11 @@
-<?php
-	$link1 = 'Click here to find Pivot in your area';
-	$link2 = "Don't Show Me This Again";
-?>
 <div class="interstitial channel-finder">
 	<div class="channel-finder-widget">
 		<?php print $pivot_channel_finder; ?>
 	</div>
 	<div class="interstitial-cta">
-		<?php print $link1; ?>
+		Click here to find Pivot in your area
 	</div>
 	<div class="interstitial-dont-show">
-		<?php print $link2; ?>
+<!--		<a href="#" class="tpmodal_close interstitial_modal_close">Don't Show Me This Again</a>-->
 	</div>
 </div>
