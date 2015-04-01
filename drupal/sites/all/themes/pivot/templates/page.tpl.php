e10c04052710b0c5fa5c6336ec86badec20f179b
Marc Goldstein <mgoldstein@takepart.com>
2015-03-31 18:13:20 -0700

    TP-2938 - add corporate logos to Pivot footer
    TP-2938 - add corporate logos to Pivot footer
    
    TP-2938 - remove old logo strip and fix copyright year

--- drupal/sites/all/themes/pivot/templates/page.tpl.php
+++ drupal/sites/all/themes/pivot/templates/page.tpl.php
@@ -1,59 +1,68 @@
 <div class="drawers">
-	<div class="snap-drawer snap-drawer-left">
-		<?php print render($page['left_drawer']); ?>
-	</div>
+    <div class="snap-drawer snap-drawer-left">
+        <?php print render($page['left_drawer']); ?>
+    </div>
 </div>
 <div id="page">
 
-  <?php
+    <?php
     $header = render($page['header']);
     if ($header):
-  ?>
+        ?>
 
-  <header id="site-header" role="banner" class="header">
+        <header id="site-header" role="banner" class="header">
 
-    <?php print $header; ?>
+            <?php print $header; ?>
 
-  </header>
-  <?php endif; ?>
-  <main id="main">
-    <div id="content" class="site-wrapper">
+        </header>
+    <?php endif; ?>
+    <main id="main">
+        <div id="content" class="site-wrapper">
 
-      <?php print render($page['preface']); ?>
+            <?php print render($page['preface']); ?>
 
-      <?php
-        $right_sidebar  = render($page['right_sidebar']);
-      ?>
+            <?php
+            $right_sidebar = render($page['right_sidebar']);
+            ?>
 
-      <div class="primary<?php print $right_sidebar ? ' with-sidebar' : '' ?>" role="main">
-        <?php print $breadcrumb; ?>
-        <a id="main-content"></a>
-        <?php print render($title_prefix); ?>
-	<?php if (isset($title) && $title): ?>
-          <h1 class="title" id="page-title"><?php print $title; ?></h1>
-        <?php endif; ?>
-        <?php print render($title_suffix); ?>
-        <?php print $messages; ?>
-        <?php global $user; ?>
-        <?php print ($user->uid != 0 ? render($tabs) : ''); ?>
-        <?php print render($page['help']); ?>
-        <?php if ($action_links): ?>
-          <ul class="action-links"><?php print render($action_links); ?></ul>
-        <?php endif; ?>
-        <?php print render($page['content']); ?>
-      </div>
+            <div class="primary<?php print $right_sidebar ? ' with-sidebar' : ''  ?>" role="main">
+                <?php print $breadcrumb; ?>
+                <a id="main-content"></a>
+                <?php print render($title_prefix); ?>
+                <?php if (isset($title) && $title): ?>
+                    <h1 class="title" id="page-title"><?php print $title; ?></h1>
+                <?php endif; ?>
+                <?php print render($title_suffix); ?>
+                <?php print $messages; ?>
+                <?php global $user; ?>
+                <?php print ($user->uid != 0 ? render($tabs) : ''); ?>
+                <?php print render($page['help']); ?>
+                <?php if ($action_links): ?>
+                    <ul class="action-links"><?php print render($action_links); ?></ul>
+                <?php endif; ?>
+                <?php print render($page['content']); ?>
+            </div>
 
-      <?php if ($right_sidebar): ?>
-      <?php print $right_sidebar; ?>
-      <?php endif; ?>
+            <?php if ($right_sidebar): ?>
+                <?php print $right_sidebar; ?>
+            <?php endif; ?>
 
-      <?php print render($page['suffix']); ?>
+            <?php print render($page['suffix']); ?>
 
-    </div><!-- /#content -->
-  </main><!-- /#main -->
+        </div><!-- /#content -->
+    </main><!-- /#main -->
 
-	<?php print render($page['prefix_footer']); ?>
-  <?php // footer will always render ?>
-  <?php print render($page['footer']); ?>
+    <?php print render($page['prefix_footer']); ?>
+    <?php // footer will always render ?>
+    <?php print render($page['footer']); ?>
+
+    <div id="corporate_logos">
+        <div id="logos_wrapper">
+            <a href="http://www.pivot.tv" class="logos-pivot" target="_blank"><img src="/sites/all/themes/pivot/images/pivot_footer.png" alt="Pivot" /></a>
+            <a href="http://www.participantmedia.com" class="logos-participantmedia" target="_blank"><img src="/sites/all/themes/pivot/images/pm_footer.png" alt="Participant Media" /></a>
+            <a href="http://www.takepart.com" class="logos-takepart" target="_blank"><img src="/sites/all/themes/pivot/images/takepart_footer.png" alt="TakePart" /></a>
+        </div>
+    </div>
 
 </div><!-- /#page -->
+
